619c68356820aea09c311c5e0f22e7b5ce9bd27a
FlashShri <shrikantdhengle3101@gmail.com>
2023-01-15 16:29:45 +0530

    adding JS with Api fetch data

---
+++ quote-generator/index.js
@@ -0,0 +1,54 @@
+
+// getting elements from html 
+const qoutEl = document.getElementById("qoute");
+const authEl = document.getElementById("author");
+const btnEl  = document.getElementById("btn");
+
+
+// API 
+const apiURl = "https://api.api-ninjas.com/v1/quotes?category=inspirational";
+const apiKey = "rlYbedSJ3QEW5WpN4ccsCA==j5lVBHlA2624JPJb";
+const apiobj = {
+    method: "GET" ,
+    headers: {
+        "x-Api-Key": apiKey
+    }
+};
+
+
+// asyns await function used to wait for getting response 
+async function getQoute(){
+   try {
+   
+    // changing the look for loading time
+    qoutEl.innerText = "loading..." ;
+    authEl.innerText = "---"
+    btnEl.disabled = "true";
+    btnEl.innerText = "loading...";
+
+
+    //fetching data from api 
+    const response = await fetch(apiURl, apiobj );
+    const data = await response.json();
+
+
+    // puttting data into elements
+    qoutEl.innerText = data[0].quote ;
+    authEl.innerText =`- ${data[0].author}` ;
+
+    // after getting succeeful enable the button
+    btnEl.disabled = false;
+    btnEl.innerText = "Get a new qoute";
+
+
+   } catch (error) {
+
+    // if error occurd
+    qoutEl.innerText = "Error!! try again later";
+    authEl.innerHTML = "<span>&#128528</span>" ;
+    btnEl.disabled = false;
+    btnEl.innerText = "Get a new qoute";
+    console.log(error);
+   }
+}
+btnEl.addEventListener("click", getQoute);
