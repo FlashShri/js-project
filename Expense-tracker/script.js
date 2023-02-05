626fe999047102a4cc5f3018e9aa899ff49b7316
FlashShri <shrikantdhengle3101@gmail.com>
2023-02-05 11:29:42 +0530

    adding styles

---
+++ Expense-tracker/script.js
@@ -0,0 +1,8 @@
+const dateEl = document.getElementById("date");
+const expEl = document.getElementById("exinput");
+const catEl = document.getElementById("categoryOption");
+const pmEl = document.getElementById("payoption");
+const amtEl = document.getElementById("amount");
+const btnEl = document.getElementById("submit");
+
+btnEl.addEventListener("click", )
